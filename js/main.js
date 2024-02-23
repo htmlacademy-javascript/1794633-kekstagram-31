3f7a461d25e75c9067990e620c9fdaadf36550c0
belovdeveloper <belovdeveloper@gmail.com>
2024-02-24 00:15:21 +0300

    Исправлена функция генерации ид-ра комментария

--- js/main.js
+++ js/main.js
@@ -21,20 +21,21 @@ function Photo(id) {
 
 function GenerateComments() {
   const MIN_COMMENTS = 0, MAX_COMMENTS = 30;
+  const MIN_COMMENT_ID = 1, MAX_COMMENT_ID = 300;
   const commentsQuantity = getRandomNumber(MIN_COMMENTS, MAX_COMMENTS);
   const comments = [];
 
   for(let i = MIN_COMMENTS; i <= commentsQuantity; i++) {
-    comments.push(new Comment(i));
+    comments.push(new Comment(createRandomIdFromRangeGenerator(MIN_COMMENT_ID, MAX_COMMENT_ID)));
   }
 
   return comments;
 }
 
-function Comment(id) {
+function Comment(IdGenerator) {
   const MIN_AVATAR_ID = 1, MAX_AVATAR_ID = 6;
 
-  this.id = id;
+  this.id = IdGenerator();
   this.avatar = `img/avatar-${getRandomNumber(MIN_AVATAR_ID, MAX_AVATAR_ID)}.svg`;
   this.message = getRandomComment();
   this.name = getRandomAuthor();
@@ -44,12 +45,28 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function createRandomIdFromRangeGenerator (min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomNumber(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      //console.error('Перебраны все числа из диапазона от ' + min + ' до ' + max);
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomNumber(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
 function getRandomTextRow(text) {
   const rows = text.split('\n');
   const randomIndex = Math.floor(Math.random() * rows.length);
   return rows[randomIndex];
 }
-
 
 function getRandomComment() {
   const TEMPLATE_TEXT =
@@ -73,4 +90,5 @@ function getRandomAuthor() {
 
   return getRandomTextRow(TEMPLATE_TEXT);
 }
+
 GeneratePhotos().forEach((photo) => console.log(photo));
